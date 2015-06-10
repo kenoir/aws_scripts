3229af08d9e9cea6f2d24722be22c2d5b4c55015
Robert Kenny <kenoir@gmail.com>
2015-06-10 09:28:57 +0100

    optionally decrement desired capacity

--- superkill.sh
+++ superkill.sh
@@ -4,10 +4,16 @@ INSTANCE_ID=$1
 ASG_NAME=`aws autoscaling describe-auto-scaling-instances --instance-ids i-0da256f4 | jq -r ".AutoScalingInstances[0].AutoScalingGroupName"`
 
 echo "Terminating $INSTANCE_ID in $ASG_NAME"
+
+if [ "$2" = "decrement" ]; then
+    echo "This action will decrement desired capacity"
+    ACTION="--should-decrement-desired-capacity"
+fi
+
 read -p "Are you sure? (y/n)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     aws ec2 modify-instance-attribute --instance-id $INSTANCE_ID --no-disable-api-termination --profile media-service
-    aws autoscaling terminate-instance-in-auto-scaling-group --instance-id $INSTANCE_ID --should-decrement-desired-capacity --profile media-service
+    aws autoscaling terminate-instance-in-auto-scaling-group --instance-id $INSTANCE_ID $ACTION --profile media-service
 fi
